4a356deabbf683c2eb47ff72ee076869aa9bdea0
ch3sney <chesney.18@osu.edu>
2023-11-02 11:45:20 -0400

    add user list

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users working!</h2>;
+  // Eventually fetch from API
+  const USERS = [
+    {
+      id: "u1",
+      name: "Luke Chesney",
+      image:
+        "https://i.guim.co.uk/img/media/26392d05302e02f7bf4eb143bb84c8097d09144b/446_167_3683_2210/master/3683.jpg?width=620&dpr=2&s=none",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
